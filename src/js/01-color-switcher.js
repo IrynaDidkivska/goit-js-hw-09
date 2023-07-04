d0c6aaa2d9c9128fa824de203551f3a8c61bb81c
Iryna Didkivska <ididkivska@gmail.com>
2023-07-04 22:12:14 +0200

    updates src

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const body = document.querySelector('body');
+const startBtnHeandler = document.querySelector('[data-start]');
+const stopBtnHeandler = document.querySelector('[data-stop]');
+let timerId = null;
+startBtnHeandler.addEventListener('click', startBtnHeandlerClick );
+stopBtnHeandler.addEventListener('click', stopBtnHeandlerClick);
+
+console.dir(startBtnHeandler);
+
+function startBtnHeandlerClick() { 
+    startBtnHeandler.disabled = true;
+    stopBtnHeandler.disabled = false;
+     timerId = setInterval(() => {
+    return body.style.backgroundColor = getRandomHexColor();
+     }, 1000)
+    console.log('Start');
+}
+
+function stopBtnHeandlerClick() { 
+    startBtnHeandler.disabled = false;
+    stopBtnHeandler.disabled = true;
+    clearInterval(timerId);
+    console.log('Stop');
+}
+
+
+
+
+
+
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const datePickerInput = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]')
+
+let targetDateTime = null;
+let timerID = null;
+startBtn.addEventListener('click', onClickStartBtn)
+startBtn.disabled = true; // Disabled start btn when page loaded
+
+
+function onClickStartBtn() {
+    timerID = setInterval(onTimerTick, 1000);
+    startBtn.disabled = true; 
+    datePickerInput.disabled = true;
+}
+
+function onTimerTick() {
+    const currentDateTime = new Date(); // Get current time
+    const diff = targetDateTime - currentDateTime; // Interval in ms between target and current time
+
+    if (diff < 0) { // Таймер повинен зупинятися, коли дійшов до кінцевої дати
+        console.log(`Stop Interval = ${timerID}`);
+        clearInterval(timerID);
+        return;
+    }
+
+    const convertedDiff = convertMs(diff); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+
+    // const { daysEl, hoursEl, minutesEl, secondsEl } = document.querySelectorAll('[data-days], [data-hours], [data-minutes], [data-seconds]');
+    
+    document.querySelector('[data-days]').textContent = addLeadingZero(convertedDiff.days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(convertedDiff.hours);
+    document.querySelector('[data-minutes]').textContent = addLeadingZero(convertedDiff.minutes);
+    document.querySelector('[data-seconds]').textContent = addLeadingZero(convertedDiff.seconds);
+}
+// перед рендерингом інтефрейсу форматує значення
+function addLeadingZero(value) { 
+    return String(value).padStart(2, "0");
+}
+
+const options = {
+  enableTime: true, // Включает выбор времени
+  time_24hr: true, // Displays time picker in 24 hour mode without AM/PM selection when enabled.
+  defaultDate: new Date(), // Sets the initial selected date(s).
+  minuteIncrement: 1, // Adjusts the step for the minute input (incl. scrolling)
+  onClose,
+};
+
+function onClose(selectedDates) { // Function(s) to trigger on every time the calendar is closed. See Events API
+    targetDateTime = selectedDates[0];
+
+    if (targetDateTime > new Date()) {
+        // if user select feature time we undisabled start button
+        startBtn.disabled = false;
+    } else {
+        // if user select past time we disabled start button
+        startBtn.disabled = true;
+        Notiflix.Notify.warning("Please choose a date in the future");
+    }
+}
+
+flatpickr(datePickerInput, options);
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
