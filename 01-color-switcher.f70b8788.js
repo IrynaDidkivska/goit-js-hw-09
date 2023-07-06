967b956e1566ae1354b4cd6e6d019c635d27dd08
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-07-06 12:52:53 +0000

    Deploying to gh-pages from @ IrynaDidkivska/goit-js-hw-09@7db10833cad3ce7dcb0008b9a4d6881ca545b158 🚀

--- 01-color-switcher.f70b8788.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");let l=null;t.addEventListener("click",(function(){t.disabled=!0,o.disabled=!1,l=setInterval((()=>e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3),console.log("Start")})),o.addEventListener("click",(function(){t.disabled=!1,o.disabled=!0,clearInterval(l),console.log("Stop")})),console.dir(t);
-//# sourceMappingURL=01-color-switcher.f70b8788.js.map
